17999800fc404b5eb2eb7cb1623cae442ab88957
Ilya Dubatovka <ilya.dubatovka@mail.ru>
2024-07-21 22:48:04 +0300

    feat: added tile recoloring

--- Assets/Scripts/GlobalConstants.cs
+++ Assets/Scripts/GlobalConstants.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class GlobalConstants
 {
     public const string GROUND_TAG = "Ground";
+    public const string TILE_TAG = "Tile";
 }
--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 
 public class TileController : MonoBehaviour
 {
+    private Grid _grid;
     private Camera _camera;
+    private Renderer[] _renderers=new Renderer[10];
 
 
     private void Awake()
     {
         _camera = Camera.main;
+        _grid = FindObjectOfType<Grid>();
+        _renderers = GetComponentsInChildren<Renderer>();
     }
 
     private void Update()
@@ -19,8 +23,23 @@ public class TileController : MonoBehaviour
         RaycastHit hitInfo;
         if (Physics.Raycast(ray,out hitInfo)&&hitInfo.transform.CompareTag(GlobalConstants.GROUND_TAG))
         {
-            transform.position = new Vector3(hitInfo.point.x, 0.2f, hitInfo.point.z);
-            //Debug.Log($"x:{hitInfo.point.x} y:{hitInfo.point.y} z:{hitInfo.point.z}");
+            var cellPosition = _grid.WorldToCell(hitInfo.point);
+            var cellCenter = _grid.GetCellCenterWorld(cellPosition);
+            transform.position=new Vector3(cellCenter.x,0.1f,cellCenter.z);
+            for (var i = 0; i < _renderers.Length; i++)
+            {
+                _renderers[i].material.color=Color.green;
+            }
+        }
+        if (Physics.Raycast(ray,out hitInfo)&&hitInfo.transform.CompareTag(GlobalConstants.TILE_TAG))
+        {
+            var cellPosition = _grid.WorldToCell(hitInfo.point);
+            var cellCenter = _grid.GetCellCenterWorld(cellPosition);
+            transform.position=new Vector3(cellCenter.x,0.1f,cellCenter.z);
+            for (var i = 0; i < _renderers.Length; i++)
+            {
+                _renderers[i].material.color=Color.red;
+            }
         }
     }
 }
